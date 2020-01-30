a2e712923b3e657c2ff446149d19a096029cc646
Guiping <Guiping_Liu@hotmail.com>
2020-01-30 20:59:58 +0800

    add demo code

---
+++ ui/src/test.js
@@ -0,0 +1,5 @@
+function a() {const b = 'aaa'; const c = b + 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'}
+
+const api = 'aip';
+
+const bad = "dafdafdafdafdafdafdafkdajfkdljfdakf;faereqrefafadfdafeoremdfldkfldakfldk ffakfldkf;dkafd lfdkalfda"
